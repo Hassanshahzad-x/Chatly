f91516e8ea782092d7eeb3551ab454b26c1687f8
Hassanshahzad-x <hassanshahzad3513@gmail.com>
2025-02-03 22:12:58 +0500

    urls.py configured

---
+++ chatly/chatbot/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+
+def chatbot(request):
+    return HttpResponse('chatbot')
+
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def logout(request):
+    pass
